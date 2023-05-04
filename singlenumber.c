aa07a2c582c8c4a73967a45349ca8bc6bfa9a8a3
sanvi2511 <132061295+sanvi2511@users.noreply.github.com>
2023-05-04 19:31:11 +0530

    Update singlenumber.c

--- singlenumber.c
+++ singlenumber.c
@@ -11,17 +11,4 @@ int main()
     d=s-sum;
     printf("The missing number is %d",d);
     return 0;
-}#include<stdio.h>
-#include<math.h>
-int main()
-{
-    int arr[]={1,2,3,4,6,7},s,sum=0,d,n=7,i;
-    s=(n*(n+1))/2;
-    for(i=0;i<n-1;i++)
-    {
-        sum=sum+arr[i];
-    }
-    d=s-sum;
-    printf("The missing number is %d",d);
-    return 0;
 }
