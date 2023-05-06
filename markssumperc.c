db13025b8a84c80b0fbe653525348ee6d7fdc4d3
sanvi2511 <132061295+sanvi2511@users.noreply.github.com>
2023-05-06 23:25:23 +0530

    Update markssumperc.c

--- markssumperc.c
+++ markssumperc.c
@@ -7,7 +7,7 @@ int main()
     printf("Input marks of Physics , Chemistry , Maths and English\n");
     scanf("%d%d%d%d%d",&p,&c,&m,&b,&e);
     s=p+c+m+b+e;
-    per=s/500;
+    per=s/5;
     printf("Sum of Marks entered : %d \n",s);
     printf("Percentage : %f",per);
     getch();
