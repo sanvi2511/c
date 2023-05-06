2fa25da213cd7ffdcb61ea4f253e42ed0b6d33eb
sanvi2511 <132061295+sanvi2511@users.noreply.github.com>
2023-05-06 23:23:43 +0530

    Add files via upload

---
+++ markssumperc.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+    int p,c,m,b,e,s;
+    float per;
+    printf("Input marks of Physics , Chemistry , Maths and English\n");
+    scanf("%d%d%d%d%d",&p,&c,&m,&b,&e);
+    s=p+c+m+b+e;
+    per=s/500;
+    printf("Sum of Marks entered : %d \n",s);
+    printf("Percentage : %f",per);
+    getch();
+}
